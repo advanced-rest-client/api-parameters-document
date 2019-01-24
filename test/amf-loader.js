72f317f2e8336c4c17bd713b7570608aaea69538
Pawel Psztyc <jarrodek@gmail.com>
2019-01-24 12:08:58 -0800

    Update: Updaitng test and bumping version to CI to kick in.

--- test/amf-loader.js
+++ test/amf-loader.js
@@ -1,9 +1,10 @@
 const AmfLoader = {};
-AmfLoader.load = function(endpointIndex, methodIndex) {
-  endpointIndex = endpointIndex || 0;
+AmfLoader.load = function(compact) {
+  const file = '/demo-api' + (compact ? '-compact' : '') + '.json';
   const url = location.protocol + '//' + location.host +
     location.pathname.substr(0, location.pathname.lastIndexOf('/'))
-    .replace('/test', '/demo') + '/demo-api.json';
+    .replace(/\/test.*/, '/demo') + file;
+
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', (e) => {
@@ -14,20 +15,7 @@ AmfLoader.load = function(endpointIndex, methodIndex) {
         reject(e);
         return;
       }
-      const enc = data[0]['http://a.ml/vocabularies/document#encodes'][0];
-      const srv = enc['http://a.ml/vocabularies/http#server'][0];
-      const base = srv['http://a.ml/vocabularies/http#variable'];
-
-      const endpoint = enc['http://a.ml/vocabularies/http#endpoint'][endpointIndex];
-      const method = endpoint['http://www.w3.org/ns/hydra/core#supportedOperation'][methodIndex];
-      const path = endpoint['http://a.ml/vocabularies/http#parameter'];
-      const expects = method['http://www.w3.org/ns/hydra/core#expects'];
-      let query;
-      if (expects) {
-        const expect = expects[0];
-        query = expect['http://a.ml/vocabularies/http#parameter'];
-      }
-      resolve([data, base, path, query]);
+      resolve(data);
     });
     xhr.addEventListener('error',
       () => reject(new Error('Unable to load model file')));
